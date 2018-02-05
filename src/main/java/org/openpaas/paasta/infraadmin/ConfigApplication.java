222d3fe596c1b420d8d843e46e7e320c279912e8
System Administrator <root@seong-ugiui-MacBook-Pro.local>
2018-02-05 13:31:02 +0900

    init

---
+++ src/main/java/org/openpaas/paasta/infraadmin/ConfigApplication.java
@@ -0,0 +1,18 @@
+package org.openpaas.paasta.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Application.
+ */
+
+
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
+
