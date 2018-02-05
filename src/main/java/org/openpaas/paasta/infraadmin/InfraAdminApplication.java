2a0db61ee4813a98f8bdb8c4f041ff7acc7d2d44
swmoon <swmoon@bluedigm.com>
2018-02-05 17:00:50 +0900

    PAAS-TA V2 - 인프라 어드민 유레카 서버 연동 완료 - 인프라 어드민 초기 구성 완료 - 세션 관리 추가 예정

--- src/main/java/org/openpaas/paasta/infraadmin/InfraAdminApplication.java
+++ src/main/java/org/openpaas/paasta/infraadmin/InfraAdminApplication.java
@@ -1,7 +1,10 @@
 package org.openpaas.paasta.infraadmin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * The type Application.
@@ -9,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
 public class InfraAdminApplication {
 
     public static void main(String[] args) {
