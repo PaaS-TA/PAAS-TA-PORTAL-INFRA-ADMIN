2e05886c39edd2dbc5cdaadde2ad1d764daced71
swmoon <swmoon@bluedigm.com>
2018-04-12 09:42:48 +0900

    InfraAdmin - Eureka로 Client 정보 가져오도록 수정중

--- src/main/java/org/openpaas/paasta/infraadmin/InfraAdminApplication.java
+++ src/main/java/org/openpaas/paasta/infraadmin/InfraAdminApplication.java
@@ -2,15 +2,18 @@ package org.openpaas.paasta.infraadmin;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * The type Application.
  */
 
-
+@EnableAutoConfiguration
+@Configuration
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
